c489a2f6db1b099bf9ae7d3d2747922de521fbd3
Dmitri Popov <operator@cv.dp-net.com>
2016-08-03 20:48:58 +0200

    separate postinst directory

--- install.sh
+++ install.sh
@@ -1,27 +1,39 @@
 #!/bin/sh
  
 # A script to install debian jessie on a KVM guest
+
+DOMAIN="dp-net.com" # See late.sh for more domain name hardcodes
  
-if [ $# -ne 1 ]
+if [ $# -lt 1 ]
 then
-	echo "Usage: $0 <guest-name>"
+	echo "Usage: $0 <guest-name> [mac-address]"
 	exit 1
 fi
+
+MAC="RANDOM"
+if [ $# -eq 2 ]
+then
+	MAC=$2
+fi
+
+tar cvfz postinst.tar.gz postinst
  
 virt-install \
 --connect=qemu:///system \
 --name=${1} \
 --ram=1024 \
---vcpus=4 \
---disk size=4,path=/var/lib/libvirt/images/${1}.img,bus=virtio,cache=none \
+--vcpus=2 \
+--disk size=16,path=/var/lib/libvirt/images/${1}.img,bus=virtio,cache=none \
 --initrd-inject=preseed.cfg \
 --initrd-inject=late.sh \
+--initrd-inject=postinst.tar.gz \
 --location http://ftp.de.debian.org/debian/dists/jessie/main/installer-amd64/ \
 --os-type=linux \
 --virt-type=kvm \
 --controller usb,model=none \
 --graphics none \
 --noautoconsole \
---network bridge=br0,mac=RANDOM,model=virtio \
---extra-args="auto=true hostname="${1}" domain=dp-net.com console=tty0 console=ttyS0,115200n8 serial"
+--network bridge=br0,mac=${MAC},model=virtio \
+--extra-args="auto=true hostname="${1}" domain="${DOMAIN}" console=tty0 console=ttyS0,115200n8 serial"
 
+rm postinst.tar.gz
--- late.sh
+++ late.sh
@@ -10,10 +10,22 @@ sed -i 's/%sudo\tALL=(ALL:ALL) ALL/%sudo\tALL=(ALL:ALL) NOPASSWD:ALL/g' /etc/sud
 # Empty message of the day.
 echo -n > /etc/motd
 
-# Install SSH key for root.
-mkdir -m700 /root/.ssh
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC6Up5b2jDreGMmMbkLtbskCdFTIrEtJLvpzCg95p2wHaEpEEu4VqvibL216Wbyeg/b9+fOnLyRV2eNZZC3gaq7IZak3STsdbZg8K7I/1ECSO3sMeACLwvNUlZLXEBP16IOIJQ3DmxWHIK4+0ndX8W2f8hrPYBVVW5Rbr5vbR27stlXwBI2ZerCxRmfhjuKz5VGvc40nUCHCe0ZrqOJumFnX8EDiu9sbutJKiMVObjpD0ENld8rm/sHiuQi2ds2uKsZOMSbtrefzfnIUf+s2/2AgUA8OsR2SMwi2xfl1iYo8SzYQCdGn12KUG5ODoowEjws9X4kbDYa5H+q9gfNFwrscrVPqqazc8NqgERXMdpuzpPhmy4EmVy+HtVwYkfDJ+k/N1Z9Tea0tm8TY952oRE4wBQnd0Wuk2ywi0UjBaZggwMteZGtVIn0saBjeej68SHD1Iex6crVQkfvNbZM+0evcCs0FqPsTLEykc4RcOUQx9V2yJLXmsIStHkZ5goE4nBtxkIGUMtP23lU0+GeLuZGO43wf3kPfKBNvNK4FdkZrxn6Vfas3IshF/x1amc6XkXY2rMqoCIyiUjvulWTcOlwNTbVQDXqBpeYCbjGwUihUU7yZl3ybLPNSjseBbxjgCo7lMdhe71WATH22lsL0Z7gpXjU8xUErJe4nBS9klVxow==" > /root/.ssh/authorized_keys
+tar -x -v -z -C/tmp -f /tmp/postinst.tar.gz
+
+# Install SSH key for pin.
+mkdir -m700 /home/pin/.ssh
+cat /tmp/postinst/authorized_keys > /home/pin/.ssh/authorized_keys
+chown -R pin:pin /home/pin/.ssh
+
+# Install collectd config.
+cp /tmp/postinst/collectd.conf /etc/collectd/
+
+# Set domain name.
+sed -i 's/127.0.1.1\t\([a-z]*\).*/127.0.1.1\t\1\.dp\-net\.com\t\1/' /etc/hosts
 
 # Remove some not essential packages.
-DEBIAN_FRONTEND=noninteractive apt-get purge -y nano gcc-4.8-base ispell laptop-detect tasksel
+DEBIAN_FRONTEND=noninteractive apt-get purge -y nano gcc-4.8-base ispell laptop-detect tasksel dictionaries-common emacsen-common
+
+# Avoid using DHCP-server provided domain name.
+sed -i 's/#supersede.*/supersede domain-name "dp-net.com";/' /etc/dhcp/dhclient.conf
 
